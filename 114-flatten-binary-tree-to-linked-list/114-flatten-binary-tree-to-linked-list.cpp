06dcb080ddd853b2b08c16c66ca273ee44715fdf
Ujjwal Anand <87986972+ujjwal-anand-0207@users.noreply.github.com>
2022-07-27 10:05:43 +0530

    Time: 3 ms (97.25%), Space: 12.7 MB (38.74%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.cpp
@@ -1,41 +1,26 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-     vector<int>n;
-    void preorder(TreeNode *root)
-    { 
-        if(root==NULL)
-            return;
-        n.push_back(root->val);
-        preorder(root->left);
-        preorder(root->right);
+    
+    TreeNode* rightmost(TreeNode* root){
+        if (root->right==NULL) return root;
+        return rightmost(root->right);
     }
+    
     void flatten(TreeNode* root) {
-       if (root==NULL)
-           return;
-        preorder(root);
-        root->val=n[0];
-        root->left=NULL;
-        root->right=NULL;
-       for(int i=1;i<n.size();++i)
-       {
-           TreeNode *temp=new TreeNode();
-           temp->val=n[i];
-           temp->left=NULL;
-           temp->right=NULL;
-           root->right=temp;
-           root=temp;
-       }
+        if (root==NULL) return;
+        TreeNode* nextright;
+        TreeNode* rightMOST;
         
+        while (root){
+            
+            if (root->left){
+                rightMOST = rightmost(root->left);
+                nextright = root->right;
+                root->right = root->left;
+                root->left=NULL;
+                rightMOST->right=nextright;
+            }
+            root=root->right;
+        }
     }
 };
